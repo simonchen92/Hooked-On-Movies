74b9538fd31df40128ea247e2438565de21df95b
simonchen92 <simon.k.chen92@gmail.com>
2020-03-18 16:46:10 -0400

    Created landing page for movies & search function
    Working display of  default movies
    Made searching function to work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect } from 'react';
+import { reducer, initialState } from '../../store/reducer/index'
+import axios from 'axios'
+import spinner from '../../assets/ajax-loader.gif'
+
 import './App.css';
 
+import Header from '../Header/Header'
+import Movie from '../Movie/Movie'
+import SearchMovie from '../SearchMovie/SearchMovie'
+
+
+
+const movieAPIURL = 'http://www.omdbapi.com/?s=man&apikey=a20929c9'
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    axios.get(movieAPIURL)
+    .then(jsonResponse => {
+      dispatch({
+        type: 'SEARCH_MOVIES_SUCCESS',
+        payload: jsonResponse.data.Search
+      })
+    })
+  }, [])
+
+  // refresh page
+  // const refreshPage = () => {
+  //   window.location.reload()
+  // }
+
+  const search = searchValue => {
+    dispatch({
+      type: 'SEARCH_MOVIES'
+    })
+
+    axios(`http://www.omdbapi.com/?s=${searchValue}&apikey=a20929c9`)
+    .then(jsonResponse => {
+      if(jsonResponse.data.Response === 'True') {
+        dispatch({
+          type: 'SEARCH_MOVIES_SUCCESS',
+          payload: jsonResponse.data.Search
+        })
+      } else {
+        dispatch({
+          type: 'SEARCH_MOVIES_FAILURE',
+          error: jsonResponse.data.Error
+        })
+      }
+    })
+  }
+
+  const { movies, errorMessage, loading } = state
+
+  const retreivedMovie = 
+    loading && !errorMessage ? (
+      <img className="spinner" src={spinner} alt="Loading spinner" />
+    ) : errorMessage ? (
+      <div className="errorMessage">{errorMessage}</div>
+    ) : (
+      movies.map((movie, index) => {
+        return <Movie key={`${index}`} movie={movie} />
+      })
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='movie-container'>
+        <Header text='Hooked On Movies' />
+        <SearchMovie search={search} />
+        <p className='App-intro'>Sharing a few of my favorite movies</p>
+        <div className="movies">{retreivedMovie}</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
