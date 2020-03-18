0a30f3f7b451b9aadb25ad25b4f8ed36276887da
simonchen92 <simon.k.chen92@gmail.com>
2020-03-18 14:10:36 -0400

    Create a search component
    Used react hook useState to set values

---
+++ src/components/SearchMovie/SearchMovie.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Search = (props) => {
+    const [searchValue, setSearchValue] = useState('')
+
+    const handleSearchInputChange = event => {
+        setSearchValue(event.target.value)
+    }
+    
+    const resetInputValue = () => {
+        setSearchValue('')
+    }
+
+    const searchFunction = event => {
+        event.preventDefault()
+        props.search(searchValue)
+        resetInputValue()
+    }
+
+    return (
+        <form className='SearchForm'>
+            <input
+                value={searchValue}
+                onChange={handleSearchInputChange}
+                type='text'
+            />
+            <input onClick={searchFunction} value='search' type='submit'/>
+        </form>
+    )
+}
+
+export default Search
