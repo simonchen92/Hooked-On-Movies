fc73c13376b100023973c090602a5beb8eaebd90
simonchen92 <simon.k.chen92@gmail.com>
2020-03-23 14:48:09 -0400

    Updates

---
+++ precache-manifest.42fd3e71ddeac427174bd7804cf50e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85272ab8b87fd2387d43ef065988e5e0",
+    "url": "/Hooked-On-Movies/index.html"
+  },
+  {
+    "revision": "afd353c4b7e0392103b3",
+    "url": "/Hooked-On-Movies/static/css/main.e8fb11a3.chunk.css"
+  },
+  {
+    "revision": "d9672a2bc577e1d6ab84",
+    "url": "/Hooked-On-Movies/static/js/2.597d3a6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Hooked-On-Movies/static/js/2.597d3a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd353c4b7e0392103b3",
+    "url": "/Hooked-On-Movies/static/js/main.cb566b5a.chunk.js"
+  },
+  {
+    "revision": "6e6e4f8bc0ff3b0e8df9",
+    "url": "/Hooked-On-Movies/static/js/runtime-main.1a9d6aab.js"
+  }
+]);
